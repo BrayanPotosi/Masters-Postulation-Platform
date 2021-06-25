ee0c0b9e8f4716749d5b7b4ed4f2de486c418b2f
Wilson Antury <ventas@antury.net>
2021-06-25 13:25:14 -0500

    [ADD] Responses interface, cath error Value error

--- src/administration/views.py
+++ src/administration/views.py
@@ -1,5 +1,4 @@
 from rest_framework.decorators import api_view, authentication_classes, permission_classes
-from rest_framework.response import Response
 from rest_framework import serializers, status, authentication, permissions
 from rest_framework.views import APIView
 from rest_framework.permissions import IsAuthenticated
@@ -11,6 +10,8 @@ from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from profiles.models import Profile
 from .serializers import CandidatesListSerializer
 
+from utils.responses import Responses
+
 @api_view(['GET'])
 @authentication_classes([authentication.TokenAuthentication])
 @permission_classes([permissions.IsAuthenticated, permissions.IsAdminUser])
@@ -20,21 +21,32 @@ def candidates_view(request):
     page_num = request.query_params.get('page')
     
     items_pp_query = request.query_params.get('ippage')
-    if items_pp_query is not None:
-        items_per_page = items_pp_query
+    try:
+        if items_pp_query is not None:
+            items_per_page = items_pp_query
+
+        profile_list = Profile.objects.all()
+        total_candidates = profile_list.count()
+        paginator = Paginator(profile_list,items_per_page)
+        try:
+            profile_list = paginator.page(page_num)
+        except PageNotAnInteger:
+            # If page is not an integer, deliver first page.
+            profile_list = paginator.page(1)
+        except EmptyPage:
+            # If page is out of range, deliver last page of results.
+            profile_list = paginator.page(paginator.num_pages)
+        data_serializer = CandidatesListSerializer(profile_list, many=True)
+        data = {   
+                "count": total_candidates,
+                "data": data_serializer.data
+                }
+        return Responses.make_response(data=data)
+    except ValueError:
+        return Responses.make_response(error=True, 
+                                        message="Server error: Value error, ippage is not a number",
+                                        status=status.HTTP_400_BAD_REQUEST)
+
     
 
-    profile_list = Profile.objects.all()
-    paginator = Paginator(profile_list,items_per_page)
-    try:
-        profile_list = paginator.page(page_num)
-    except PageNotAnInteger:
-        # If page is not an integer, deliver first page.
-        profile_list = paginator.page(1)
-    except EmptyPage:
-        # If page is out of range, deliver last page of results.
-        profile_list = paginator.page(paginator.num_pages)
-
-    data_serializer = CandidatesListSerializer(profile_list, many=True)
-
-    return Response(data_serializer.data)
+    
