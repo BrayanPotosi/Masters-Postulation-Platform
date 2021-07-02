bd458f904cf3999ddad9f14285e06a8c1ccd011f
Brayan Potosi <bp990308@hotmail.com>
2021-07-02 00:53:24 -0500

    [UPDATE] Change pdf view to administrator app

--- src/master_postulation_prj/urls.py
+++ src/master_postulation_prj/urls.py
@@ -27,5 +27,4 @@ urlpatterns = [
     path('api/v1/', include('djoser.urls.authtoken')),
     path('api/v1/profile/', include('profiles.urls')),
     path('api/v1/administration/', include('administration.urls')),
-    path('api/v1/reports/', include('reports.urls'))
 ]
--- src/reports/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from .views import report_users
-
-app_name = 'reports'
-
-urlpatterns = [
-    path('users/', report_users, name='users')
-]
