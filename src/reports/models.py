1e8e4b634b4cd5e518aff90b121b3cd9b18319b0
Brayan Potosi <bp990308@hotmail.com>
2021-07-03 02:21:07 -0500

    [UPDATE] Refactoring application models

--- src/reports/models.py
+++ src/reports/models.py
@@ -1,6 +1,8 @@
-from django.db import models
-from django.contrib.auth.models import User
+# Django
 from django.contrib.auth import get_user_model
+from django.contrib.auth.models import User
+from django.db import models
+
 
 class AdminLog(models.Model):
     admin = models.ForeignKey(get_user_model(), null=True, on_delete=models.SET_NULL)
@@ -14,7 +16,7 @@ class AdminLog(models.Model):
 
 
 class CandidateLog(models.Model):
-    candidate = models.ForeignKey(get_user_model(), null=True,on_delete=models.SET_NULL)
+    candidate = models.ForeignKey(get_user_model(), null=True, on_delete=models.SET_NULL)
     start_session = models.DateTimeField()
     end_session = models.DateTimeField()
     device = models.CharField(max_length=25)
@@ -22,12 +24,14 @@ class CandidateLog(models.Model):
     def __str__(self):
         return f'{self.candidate}, {self.device}'
 
+
 class HasJobWeight(models.Model):
     has_job = models.BooleanField(default=False)
     weight = models.PositiveIntegerField(default=0)
 
     def __str__(self) -> str:
         return f'{self.has_job}-{self.weight}'
+
 
 class AgeWeight(models.Model):
     age_years_start = models.PositiveIntegerField(default=0)
@@ -37,7 +41,8 @@ class AgeWeight(models.Model):
     def __str__(self) -> str:
         return f'{self.age_years_start} to {self.age_years_end}-{self.weight}'
 
-class ExperinceWeight(models.Model):
+
+class ExperienceWeight(models.Model):
     exp_months_start = models.PositiveIntegerField(default=0)
     exp_months_end = models.PositiveIntegerField(default=0)
     weight = models.PositiveIntegerField(default=0)
