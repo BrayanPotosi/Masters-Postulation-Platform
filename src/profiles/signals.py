8a01a781e811fefad9b8d88a599607a3d43dbbc0
Brayan Potosi <bp990308@hotmail.com>
2021-07-03 02:29:24 -0500

    [UPDATE] Refactoring signals on: profiles app

--- src/profiles/signals.py
+++ src/profiles/signals.py
@@ -1,12 +1,17 @@
-from .models import Profile
-from administration.models import Score
+# Django
 from django.contrib.auth import get_user_model
 from django.db.models.signals import post_save
 from django.dispatch import receiver
+
+# Djoser
 # from djoser.signals import user_registered
 
+# Models
+from administration.models import Score
+from .models import Profile
 
 User = get_user_model()
+
 
 # @receiver(user_registered)
 # def post_save_handler(user, request, **kwargs):
@@ -21,10 +26,10 @@ def post_save_create_profile(sender, instance, created, **kwargs):
         if instance.is_staff == False and instance.email:
             Profile.objects.create(user=instance)
 
+
 @receiver(post_save, sender=Profile)
 def post_save_create_job_status(sender, instance, created, **kwargs):
     if created:
         if instance.score is None:
             instance.score = Score.objects.create()
             instance.save()
-
