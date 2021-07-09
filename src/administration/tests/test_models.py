affb73fcf09f46022326d8649a3a163fc6f36bc4
Brayan Potosi <bp990308@hotmail.com>
2021-07-09 05:39:06 -0500

    [ADD] signal tests in: profile apps

---
+++ src/administration/tests/test_models.py
@@ -0,0 +1,2 @@
+# Django
+from django.test import TestCase
---
+++ src/profiles/tests/test_signals.py
@@ -0,0 +1,20 @@
+# Django
+from django.test import TestCase
+
+# Mommy
+from model_mommy import mommy
+from model_mommy.recipe import Recipe, foreign_key
+
+# Models
+from ..models import Profile
+
+
+class CreateUserProfileTest(TestCase):
+    """Check if a user's profile is created when initializing an instance"""
+
+    def setUp(self):
+        self.user = mommy.make('profiles.User', _fill_optional=True)
+        self.profile = Profile.objects.last()
+
+    def test_if_user_profile_is_created(self):
+        self.assertEqual(self.user.id, self.profile.user_id)
